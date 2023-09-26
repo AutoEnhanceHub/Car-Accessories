ce0b1996586786eeb3c2e6fd9966164cdeedd731
assad33 <i.a.s.assad33@gmail.com>
2023-09-26 14:33:34 +0300

    -m"sss!"

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,7 @@ public class main {
     public static void main(String []arg){
         System.out.println("hiwdsafs");
         int s=0;
+        System.out.println("abdullah daher");
+        String s1="gereof";
     }
 }
