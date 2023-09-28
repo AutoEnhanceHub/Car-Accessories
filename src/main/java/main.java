b9c3b3ed8a8ab67b7ce36cfffc5514eee2928b50
abdallah-daher <s12116027@stu.najah.edu>
2023-09-28 20:12:54 +0300

    abdmessage1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,5 @@
 public class main {
-    public static void main(String []arg){
+    public static void main(String[] args){
         System.out.println("system");
         int s=0;
         System.out.println("abdullah daher");
