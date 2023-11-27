936195473505c9fb560b8a872b6444f130865ca6
Ibrahim-Asad <145121314+Ibrahim-Asad@users.noreply.github.com>
2023-11-27 19:44:55 +0200

    Update Mailing.java

--- src/main/java/carAccessories/Mailing.java
+++ src/main/java/carAccessories/Mailing.java
@@ -27,7 +27,8 @@ public class Mailing {
 
     }
 
-    public void sendVerificationCode(){
+    private void sending(String Subject, String Text){
+
         try {
             Properties properties = System.getProperties();
             properties.put("mail.smtp.host", "smtp.gmail.com");
@@ -41,72 +42,35 @@ public class Mailing {
                     return new PasswordAuthentication("accessoriescar378@gmail.com","zxrl yldn nguy xhny");
                 }
             });
-            session.setDebug(false);
+            session.setDebug(true);
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to,false));
-            message.setSubject("This is your verification code for car-accessories");
-            message.setText("Your code is "+verificationCode+ "\nPlease don't share this code with anyone");
+            message.setSubject(Subject);
+            message.setText(Text);
             Transport.send(message);
         }
         catch (MessagingException m){
             m.printStackTrace();
         }
+
+        
+    }
+
+    public void sendVerificationCode(){
+      String Subject = "This is your verification code for car-accessories";
+      String Text = "Your code is "+verificationCode +"\n"+"Please don't share this code with anyone";
+     sending(Subject,Text);
+      
     }
     public void resetPassword(){
-
-        try {
-            Properties properties = System.getProperties();
-            properties.put("mail.smtp.host", "smtp.gmail.com");
-            properties.put("mail.smtp.port", "587");
-            properties.put("mail.smtp.auth", "true");
-            properties.put("mail.smtp.starttls.enable", "true");
-
-            Session session = Session.getDefaultInstance(properties,new javax.mail.Authenticator(){
-                @Override
-                protected  PasswordAuthentication getPasswordAuthentication(){
-                    return new PasswordAuthentication("accessoriescar378@gmail.com","zxrl yldn nguy xhny");
-                }
-            });
-            session.setDebug(true);
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to,false));
-            message.setSubject("This is your code to reset new password");
-            message.setText("Your code is "+verificationCode);
-            message.setText("Please don't share this code with anyone");
-            Transport.send(message);
-        }
-        catch (MessagingException m){
-            m.printStackTrace();
-        }
+        String Subject = "This is your code to reset new password";
+        String Text = "Your code is "+verificationCode +"\n"+"Please don't share this code with anyone";
+        sending(Subject,Text);
 
     }
-    public void sendEmail(String subject,String message1) {
-        try {
-            Properties properties = System.getProperties();
-            properties.put("mail.smtp.host", "smtp.gmail.com");
-            properties.put("mail.smtp.port", "587");
-            properties.put("mail.smtp.auth", "true");
-            properties.put("mail.smtp.starttls.enable", "true");
-
-            Session session = Session.getDefaultInstance(properties,new javax.mail.Authenticator(){
-                @Override
-                protected  PasswordAuthentication getPasswordAuthentication(){
-                    return new PasswordAuthentication("accessoriescar378@gmail.com","zxrl yldn nguy xhny");
-                }
-            });
-            session.setDebug(true);
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to,false));
-            message.setSubject(subject);
-            message.setText(message1);
-            Transport.send(message);
-        }
-        catch (MessagingException m){
-            m.printStackTrace();
-        }
+    public void sendEmail(String subject,String Text) {
+       sending(Subject,Text);
     }
 
 
