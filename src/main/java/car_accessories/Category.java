7426bc0c47c926acc3828243e139902ba254cc1d
abdallah-daher <145098126+abdallah-daher@users.noreply.github.com>
2023-11-27 22:02:02 +0200

    Update Category.java

--- src/main/java/car_accessories/Category.java
+++ src/main/java/car_accessories/Category.java
@@ -1,12 +1,12 @@
 package car_accessories;
-
+import java.util.List;
 import java.util.ArrayList;
 
 public class Category {
 public String name;
-public ArrayList<product> products;
+public List<product>products=new ArrayList<>();
  public Category(String name){
      this.name=name;
-     products=new ArrayList<product>();
+     
  }
 }
